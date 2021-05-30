83881fc2c3f0b1333b9807826e59059a1fdbf75f
ruvin@simct.com <Empirestate162#>
2021-05-30 15:06:53 +0530

    first

---
+++ sensor.js
@@ -0,0 +1,45 @@
+
+ client = new Messaging.Client("broker.mqttdashboard.com",8000,"ruvin");
+ 
+ //Connect Options
+      var options = {
+        //Gets Called if the connection has sucessfully been established
+        onSuccess: function () {
+         console.log("Connected");
+		  client.subscribe("testtopic/laser/pos", {qos: 2});
+          console.log("Subscribed");
+        },
+        //Gets Called if the connection could not be established
+        onFailure: function (message) {
+          console.log("Connection failed: " + message.errorMessage);
+        }
+      };
+
+     
+	
+	//Gets  called if the websocket/mqtt connection gets disconnected for any reason
+      client.onConnectionLost = function (responseObject) {
+        //Depending on your scenario you could implement a reconnect logic here
+        console.log("connection lost: " + responseObject.errorMessage);
+      };
+	  var messages ="";
+      client.onMessageArrived = function (message) {
+
+
+        //Do something with the push message you received
+        if (message.destinationName === "testtopic/laser/pos") {
+          
+           console.log("return value for sensor" +message.payloadString);
+		   messages = messages +"</br>" + message.payloadString;
+		
+		   var x = document.getElementById('textArea');
+		   x.value +=  message.payloadString+ '\r\n';
+            
+            
+
+        } 
+
+
+      };
+	  
+	   client.connect(options);
